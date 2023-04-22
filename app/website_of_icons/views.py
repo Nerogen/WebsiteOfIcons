912330686a23c83c79fac204d5c21ac36bccee5c
Nero <14k21096@gmail.com>
2023-04-22 22:57:02 +0300

    update

--- app/website_of_icons/views.py
+++ app/website_of_icons/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import Form
 from django.shortcuts import render
 from .models import Icons
 
@@ -6,9 +8,23 @@ def main(request):
     return render(request=request, template_name="main.html")
 
 
+def get(request):
+    context = {
+        'form': UserCreationForm()
+    }
+    return render(request, context)
+
+
 def home(request):
-    icons = Icons.objects.all()
-    return render(request=request, template_name="icons.html", context={"icons": icons})
+    if request.method == "POST":
+        icons = Icons.objects.all()
+        subject_matter = request.POST.get('subject_matter')
+        icons = [icon for icon in icons if subject_matter == str(icon)]
+        return render(request=request, template_name="icons.html", context={"icons": icons})
+    else:
+        icons = request.GET.get('subject_matter')
+        print(icons)
+        return render(request=request, template_name="icons.html")
 
 
 def login(request):
